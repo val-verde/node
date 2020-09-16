7dd01208c2cadd8357788eb01e69e31a1f36f457
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-09-16 16:19:20 -0700

    Don't include execinfo.h on android.

--- deps/v8/src/base/debug/stack_trace_posix.cc
+++ deps/v8/src/base/debug/stack_trace_posix.cc
@@ -26,7 +26,9 @@
 #include <vector>
 
 #if V8_LIBC_GLIBC || V8_LIBC_BSD || V8_LIBC_UCLIBC || V8_OS_SOLARIS
+#if !defined(__ANDROID__)
 #define HAVE_EXECINFO_H 1
+#endif
 #endif
 
 #if HAVE_EXECINFO_H
